6c511142e53c6ba00c447701d929e9476ed1d400
Baptistin BOILOT <baptistin@boilot.fr>
2023-02-17 16:36:57 +0100

    feat: initial print component
    Signed-off-by: Baptistin BOILOT <baptistin.boilot@epita.fr>

--- src/probe.bpf.c
+++ src/probe.bpf.c
@@ -1,6 +1,8 @@
 #include "vmlinux.h"
 #include "bpf/bpf_helpers.h"
 #include "bpf/bpf_tracing.h"
+#include "bpf/bpf_core_read.h"
+
 #include "../include/iotrace/probe.h"
 
 char __license[] SEC("license") = "Dual MIT/GPL";
@@ -8,53 +10,63 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 const volatile pid_t tracee_pid = 0;
 
 struct {
-  __uint(type, BPF_MAP_TYPE_RINGBUF);
-  __uint(max_entries, 1 << 24);
+	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, 1 << 24);
 } messages SEC(".maps");
 
 SEC("tracepoint/io_uring/io_uring_complete")
-int io_uring_complete(struct trace_event_raw_io_uring_complete *event) {
-  u64 pid;
-  struct message *msg;
-  struct io_uring_sqe req = {0};
+int io_uring_complete(struct trace_event_raw_io_uring_complete *event)
+{
+	u64 pid;
+	struct message *msg;
+	struct io_kiocb iocb = { 0 };
 
-  pid = bpf_get_current_pid_tgid() >> 32;
-  if (pid != tracee_pid)
-    return 0;
+	pid = bpf_get_current_pid_tgid() >> 32;
+	if (pid != tracee_pid)
+		return 0;
 
-  msg = bpf_ringbuf_reserve(&messages, sizeof(struct message), 0);
-  if (!msg)
-    return 0;
-  msg->res = event->res;
-  msg->user_data = event->user_data;
-  bpf_probe_read_user(&req, sizeof(struct io_uring_sqe), event->req);
-  msg->opcode = req.opcode;
-  msg->type = IO_URING_COMPLETE;
+	if (bpf_probe_read_kernel(&iocb, sizeof(struct io_kiocb), event->req) <
+	    0)
+		return 0;
 
-  bpf_ringbuf_submit(msg, 0);
-  return 0;
+	msg = bpf_ringbuf_reserve(&messages, sizeof(struct message), 0);
+	if (!msg)
+		return 0;
+	msg->type = IO_URING_COMPLETE;
+	msg->opcode = iocb.opcode;
+	msg->fd = 0;
+	msg->user_data = event->user_data;
+	msg->buf_index = iocb.buf_index;
+
+	bpf_ringbuf_submit(msg, 0);
+	return 0;
 }
 
-
 SEC("tracepoint/io_uring/io_uring_submit_sqe")
-int io_uring_submit_sqe(struct trace_event_raw_io_uring_submit_sqe *event) {
-  u64 pid;
-  struct message *msg;
-  struct io_uring_sqe req = {0};
+int io_uring_submit_sqe(struct trace_event_raw_io_uring_submit_sqe *event)
+{
+	u64 pid;
+	struct message *msg;
+	struct io_kiocb iocb = { 0 };
 
-  pid = bpf_get_current_pid_tgid() >> 32;
-  if (pid != tracee_pid)
-    return 0;
+	pid = bpf_get_current_pid_tgid() >> 32;
+	if (pid != tracee_pid)
+		return 0;
 
-  msg = bpf_ringbuf_reserve(&messages, sizeof(struct message), 0);
-  if (!msg)
-    return 0;
-  msg->res = 0;
-  msg->user_data = event->user_data;
-  bpf_probe_read_user(&req, sizeof(struct io_uring_sqe), event->req);
-  msg->opcode = req.opcode;
-  msg->type = IO_URING_SUBMIT_SQE;
+	if (bpf_probe_read_kernel(&iocb, sizeof(struct io_kiocb), event->req) <
+	    0)
+		return 0;
 
-  bpf_ringbuf_submit(msg, 0);
-  return 0;
+	msg = bpf_ringbuf_reserve(&messages, sizeof(struct message), 0);
+	if (!msg)
+		return 0;
+
+	msg->type = IO_URING_SUBMIT_SQE;
+	msg->opcode = event->opcode;
+	msg->fd = iocb.cqe.fd;
+	msg->user_data = event->user_data;
+	msg->buf_index = BPF_CORE_READ(iocb.ctx, sq_sqes, opcode);
+
+	bpf_ringbuf_submit(msg, 0);
+	return 0;
 }
